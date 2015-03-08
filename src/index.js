a40ad1c6db5990e08b851e19a9b9a0e6ddb294df
Clemens <admin@cortys.de>
2015-03-08 22:52:34 +0100

    created extenal nwa index
    nwa now just exposes an exposal method and a „serve“ helper object.

---
+++ src/index.js
@@ -0,0 +1,31 @@
+var Api = require("./Api"),
+	Binding = require("./Binding"),
+	helpers = require("./helpers");
+
+function nwa(object, router, closer) {
+
+	// An object of the form { router:[function], closer:[function] } can be used as well:
+	if(closer === undefined && typeof router === "object") {
+		if(!Array.isArray(router)) {
+			router = router.router;
+			closer = router.closer;
+		}
+		else {
+			closer = [];
+			router.forEach(function(i, v) {
+				if(typeof v !== "object")
+					throw new TypeError("router has to be of type 'function', 'object' or arrays of such.");
+				router[i] = v.router;
+				closer.push(v.closer);
+			});
+		}
+	}
+
+	Binding.bind(object, helpers.fallthrough(router), helpers.fallthrough(closer));
+
+	return new Api(object);
+}
+
+nwa.serve = helpers.serve;
+
+module.exports = nwa;
