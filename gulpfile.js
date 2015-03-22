b36c74f92a0c92737af38c29a63d8f55c34195b9
Clemens <admin@cortys.de>
2015-03-22 02:47:35 +0100

    transitioning from grunt+jasmine to gulp+mocha

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp"),
+	mocha = require("gulp-mocha");
+
+gulp.task("mocha", function() {
+	return gulp.src(["test/*.test.js"]).pipe(mocha({
+		//"use_strict": true
+	}));
+});
+
+gulp.task("watch-mocha", function() {
+	gulp.watch(["src/**", "test/**"], ["mocha"]);
+});
+
+gulp.task("default", ["mocha", "watch-mocha"]);
