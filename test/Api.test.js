caf61529754247f477122dee9920543c36d1c4ac
Clemens <admin@cortys.de>
2015-03-25 23:52:51 +0100

    more Api test cases

--- test/Api.test.js
+++ test/Api.test.js
@@ -10,7 +10,10 @@ describe("Api", function() {
 			a: 1,
 			b: 2,
 			c: 3
-		}, function() {}, function(key) {
+		}, function(a) {
+			if(a)
+				return this.object;
+		}, function(key) {
 			if(!(key in this.value))
 				throw new Error(key + " not found.");
 			return this.value[key];
@@ -18,11 +21,38 @@ describe("Api", function() {
 		api = new Api(object);
 
 	describe("#route()", function() {
-
+		it("should return an Api", function() {
+			expect(api.route()).to.be.an(Api);
+		});
 	});
 
 	describe("#close()", function() {
-
+		it("should return a Promise", function() {
+			expect(api.close()).to.be.a(Promise);
+		});
+		it("should resolve with the requested data", function() {
+			return Promise.all([
+				api.close("a"),
+				api.close("b"),
+				api.close("c")
+			]).then(function(result) {
+				expect(result).to.eql([1, 2, 3]);
+			});
+		});
+		it("should reject incorrect requests", function() {
+			return Promise.all([
+				api.close("d").then(function() {
+					expect().fail("This request should have thrown.");
+				}, function(err) {
+					expect(err.message).to.be("d not found.");
+				}),
+				api.close().then(function() {
+					expect(undefined).fail("This request should have thrown.");
+				}, function(err) {
+					expect(err.message).to.be("undefined not found.");
+				})
+			]);
+		});
 	});
 
 	describe("#object", function() {
