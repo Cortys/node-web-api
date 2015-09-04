1d289b4a1b22c476ac5be4fd92e64542bbcce2fe
Clemens Damke <admin@cortys.de>
2015-09-05 00:33:22 +0200

    0.5.5: added exposed error types

--- src/index.js
+++ src/index.js
@@ -13,4 +13,25 @@ owe.reroute = require("./reroute");
 // Chains multiple router and/or closer functions to one fallthrough function:
 owe.chain = require("./chain");
 
+// Contains aliases for the default errors, exposing its message:
+function subclassError(error) {
+	return class extends error {
+		constructor(msg) {
+			super(msg);
+
+			owe.resource(this, {
+				expose: true
+			});
+		}
+	};
+}
+
+owe.exposed = {
+	Error: subclassError(Error),
+	TypeError: subclassError(TypeError),
+	ReferenceError: subclassError(ReferenceError),
+	RangeError: subclassError(RangeError),
+	SyntaxError: subclassError(SyntaxError)
+};
+
 module.exports = owe;
